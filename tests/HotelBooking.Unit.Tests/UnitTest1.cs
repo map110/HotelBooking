b89f8da5a0c13208689f2fe223daa67d5a490e8e
m.peykanfar <m.peykanfar@PEYKANFAR-PC>
2026-03-29 18:17:44 +0330

    ایجاد فایل های پروژه تست

---
+++ tests/HotelBooking.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HotelBooking.Unit.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
